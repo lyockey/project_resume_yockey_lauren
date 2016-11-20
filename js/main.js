cda823086eeed93604b8e788e267a31d987ae4fd
Lauren Yockey <laurenyockey@gmail.com>
2016-11-19 21:25:47 -0600

    fix plugin bugs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+/* global ProgressBar, Waypoint  */
+
 $(document).ready(function () {
 
-    // $('body').animatescroll({
-    //     scrollSpeed: 3000,
-    //     easing: 'easeOutBounce'
-    // });
+    $('nav').on('click', 'a', function (event) {
+        event.preventDefault();
+
+        $(this.hash).animatescroll({
+            scrollSpeed: 3000,
+            easing: 'easeOutBounce'
+        });
+    });
 
     $.toast({
         text: 'I also have accounts on <a href="https://www.linkedin.com/in/laurenyockey" target="_blank">LinkedIn</a> and <a href="https://www.github.com/lyockey" target="_blank">Github</a>', // Text that is to be shown in the toast
@@ -18,195 +24,108 @@ $(document).ready(function () {
         loader: false,  // Whether to show loader or not. True by default
     });
 
-});
+    /////////////// SKILLS BARS //////////////////
 
-/////////////// SKILLS BARS //////////////////
+    /////// LANGUAGES //////////
 
-/////// LANGUAGES //////////
+    var lang = {
+        strokeWidth: 2,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#ffea82',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '100%'}
+    };
 
-//XHTML
-var bar1 = new ProgressBar.Line('#lang1', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffea82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    //XHTML
+    var bar1 = new ProgressBar.Line('#lang1', lang);
 
-bar1.animate(1.0);  // Number from 0.0 to 1.0
+    //HTML5
+    var bar2 = new ProgressBar.Line('#lang2', lang);
 
+    //CSS
+    var bar3 = new ProgressBar.Line('#lang3', lang);
 
-//HTML5
-var bar2 = new ProgressBar.Line('#lang2', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffea82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    //JS
+    var bar4 = new ProgressBar.Line('#lang4', lang);
 
-bar2.animate(1.0);  // Number from 0.0 to 1.0
+    //jQuery
+    var bar5 = new ProgressBar.Line('#lang5', lang);
 
-//CSS
-var bar3 = new ProgressBar.Line('#lang3', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffea82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
-
-bar3.animate(0.9);  // Number from 0.0 to 1.0
-
-//JS
-var bar4 = new ProgressBar.Line('#lang4', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffea82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
-
-bar4.animate(0.5);  // Number from 0.0 to 1.0
-
-//jQuery
-var bar5 = new ProgressBar.Line('#lang5', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffea82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
-
-bar5.animate(0.55);  // Number from 0.0 to 1.0
-
-//PHP
-var bar6 = new ProgressBar.Line('#lang6', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ffea82',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
-
-bar6.animate(0.35);  // Number from 0.0 to 1.0
+    //PHP
+    var bar6 = new ProgressBar.Line('#lang6', lang);
 
 
-////////// SOFTWARE //////////////
-// PHOTOSHOP
-var bar7 = new ProgressBar.Line('#design1', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#1a75ff',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    ////////// SOFTWARE //////////////
+    var software = {
+        strokeWidth: 2,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#1a75ff',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '100%'}
+    };
 
-bar7.animate(0.75);  // Number from 0.0 to 1.0
+    // PHOTOSHOP
+    var bar7 = new ProgressBar.Line('#design1', software);
 
-//INDESIGN
-var bar8 = new ProgressBar.Line('#design2', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#1a75ff',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    //INDESIGN
+    var bar8 = new ProgressBar.Line('#design2', software);
 
-bar8.animate(0.95);  // Number from 0.0 to 1.0
-
-//ILLUSTRATOR
-var bar9 = new ProgressBar.Line('#design3', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#1a75ff',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
-
-bar9.animate(0.78);  // Number from 0.0 to 1.0
+    //ILLUSTRATOR
+    var bar9 = new ProgressBar.Line('#design3', software);
 
 
-/////// DIGITAL MARKETING //////
-//SEO
-var bar10 = new ProgressBar.Line('#dig1', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ff0000',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    /////// DIGITAL MARKETING //////
 
-bar10.animate(1.0);  // Number from 0.0 to 1.0
+    var digital = {
+        strokeWidth: 2,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#ff0000',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '100%'}
+    };
 
-//SEM
-var bar11 = new ProgressBar.Line('#dig2', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ff0000',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    //SEO
+    var bar10 = new ProgressBar.Line('#dig1', digital);
 
-bar11.animate(0.7);  // Number from 0.0 to 1.0
+    //SEM
+    var bar11 = new ProgressBar.Line('#dig2', digital);
 
-//ANALYTICS
-var bar12 = new ProgressBar.Line('#dig3', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ff0000',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    //ANALYTICS
 
-bar12.animate(0.75);  // Number from 0.0 to 1.0
+    var bar12 = new ProgressBar.Line('#dig3', digital);
 
-//SOCIAL
-var bar13 = new ProgressBar.Line('#dig4', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ff0000',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    //SOCIAL
+    var bar13 = new ProgressBar.Line('#dig4', digital);
 
-bar13.animate(0.9);  // Number from 0.0 to 1.0
+    //COPY
+    var bar14 = new ProgressBar.Line('#dig5', digital);
 
-//COPY
-var bar14 = new ProgressBar.Line('#dig5', {
-    strokeWidth: 2,
-    easing: 'easeInOut',
-    duration: 1400,
-    color: '#ff0000',
-    trailColor: '#eee',
-    trailWidth: 1,
-    svgStyle: {width: '100%', height: '100%'}
-});
+    ///////// TRIGGER ANIMATION ON SCROLL (WAYPOINTS PLUGIN)////////
 
-bar14.animate(1.0);  // Number from 0.0 to 1.0
+    var inview = new Waypoint.Inview({
+        element: $('.skills')[0],
+        entered: function () {
+            bar1.animate(1.0);
+            bar2.animate(1.0);
+            bar3.animate(0.9);
+            bar4.animate(0.5);
+            bar5.animate(0.55);
+            bar6.animate(0.35);
+            bar7.animate(0.75);
+            bar8.animate(0.95);
+            bar9.animate(0.78);
+            bar10.animate(1.0);
+            bar11.animate(0.7);
+            bar12.animate(0.75);
+            bar13.animate(0.9);
+            bar14.animate(1.0);
+        },
+    });
+
+}); //END DOC.READY
